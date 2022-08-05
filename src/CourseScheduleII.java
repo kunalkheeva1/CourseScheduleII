a154a0438cacdb859b349a4626bfa0583735e97a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-04 20:23:20 -0700

    refer to the comments for the better details

--- src/CourseScheduleII.java
+++ src/CourseScheduleII.java
@@ -4,12 +4,17 @@ import java.util.Queue;
 
 public class CourseScheduleII {
 
-
+    //method int which 2d array states that the subject b needs to be done first in order to do a
+    // lets get into the list of list first, like, what subjects will be due after it
     static int[] courseSchedule(int n, int prerequisite [][]){
+        //get a list of list and store all the elements in it
         ArrayList[] adj = new ArrayList[n];
         for(int i =0; i<n; i++){
             adj[i]= new ArrayList();
         }
+
+        // get an array to return answer and one array to store the indegrees of the elements
+        //indegree states that how many subjects can reach a given particular one
         int []ans = new int[n];
         int index =0, count=0;
 
@@ -18,6 +23,10 @@ public class CourseScheduleII {
             inDegree[i]=0;
         }
 
+
+        //now take all the first idexes as a and second ones as b
+        //which means if I do b, then how many a subjects are open to me.
+        //also increase the indegree of all the a's as we will be approaching them after doing b
         for(int i=0; i<prerequisite.length; i++){
             int a = prerequisite[i][0];
             int b = prerequisite[i][1];
@@ -26,7 +35,7 @@ public class CourseScheduleII {
         }
 
 
-
+        //if the indegree is 0 then store it in the queue
         Queue<Integer> q = new LinkedList<>();
         for(int i=0; i<n; i++){
             if(inDegree[i]==0){
@@ -34,6 +43,12 @@ public class CourseScheduleII {
             }
         }
 
+
+        //until the queue is empty, delete the element store it in answer,
+        //check its connections, and remove there inDegrees, as the element is removed,
+        //so it wont be acting as the connection with them
+        // also take count int to measure the numbers, as it could be empty or infinity arrays
+        // only return if the count is n
         while(!q.isEmpty()){
          int current = q.remove();
          ans[index] = current;
